c1c3a9c300e9d39a691be21a0d089d7529e16e58
bowman224 <dbowman123@gmail.com>
2014-09-04 19:40:03 -0400

    Set up proxy to point to API endpoint

--- gulpfile.js
+++ gulpfile.js
@@ -78,11 +78,14 @@ gulp.task('default', ['clean'], function () {
 
 gulp.task('connect', function () {
     var connect = require('connect');
+    var proxy = require('proxy-middleware');
+    var url = require('url');
     var app = connect()
         .use(require('connect-livereload')({ port: 35729 }))
         .use(connect.static('app'))
         .use(connect.static('.tmp'))
-        .use(connect.directory('app'));
+        .use(connect.directory('app'))
+        .use('/api', proxy(url.parse('http://www.memory.lossur.es/wp/')));
 
     require('http').createServer(app)
         .listen(9000)
