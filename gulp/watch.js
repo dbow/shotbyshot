39610323a6b34d1ffd30eddab5407979da16a94b
bowman224 <dbowman123@gmail.com>
2014-09-07 15:44:14 -0400

    Livereload watches component SCSS too

--- gulp/watch.js
+++ gulp/watch.js
@@ -3,7 +3,8 @@
 var gulp = require('gulp');
 
 gulp.task('watch', ['wiredep', 'styles'] ,function () {
-  gulp.watch('app/styles/**/*.scss', ['styles']);
+  gulp.watch(['app/styles/**/*.scss',
+              'app/scripts/components/**/*.scss'], ['styles']);
   gulp.watch('app/scripts/**/*.js', ['scripts']);
   gulp.watch('app/images/**/*', ['images']);
   gulp.watch('bower.json', ['wiredep']);
