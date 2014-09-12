57f74d163eec53fb9117ce2687179b745ee31ec6
bowman224 <dbowman123@gmail.com>
2014-09-11 22:56:18 -0400

    Remove inaccessible/unused code from slidemanager

--- app/scripts/directives/slidemanager-directive.js
+++ app/scripts/directives/slidemanager-directive.js
@@ -87,35 +87,16 @@ var Scroller = {
 
       if (slide.type === 'background') {
         bg = slide;
-
-      } else if (slide.type === 'author') {
       }
     }, this);
 
     console.log('slides', this.slides);
-
-    return;
-    this.$slides.css({
-      height: this.height
-    });
-
-    return;
-
-    angular.forEach(this.slides, function (div) {
-      angular.element(div).css({paddingTop: this.doubleHeight + 'px'});
-    }, this);
-
-    angular.element(this.annotationDivs).find('p').css({
-      height: this.height + 'px'
-    });
   },
 
   onscroll: function () {
     var currentY = window.scrollY;
     var slideDistance;
-    var slide;
-    var index;
-    var i;
+    var currentSlideIndex;
 
     if (!this.slides || !this.slides.length || currentY === this.lastY) {
       return window.requestAnimationFrame(this.boundOnscroll);
@@ -123,7 +104,7 @@ var Scroller = {
 
     angular.forEach(this.slides, function (slide, i) {
       if (currentY > slide.top && currentY < slide.bottom) {
-        index = i;
+        currentSlideIndex = i;
         this.currentSlide = slide;
       }
       slide.inView = currentY > slide.top || currentY < slide.bottom;
@@ -133,8 +114,8 @@ var Scroller = {
       return window.requestAnimationFrame(this.boundOnscroll);
     }
 
-    var nextSlide = this.slides[index + 1];
-    var previousSlide = this.slides[index - 1];
+    var nextSlide = this.slides[currentSlideIndex + 1];
+    var previousSlide = this.slides[currentSlideIndex - 1];
     var props;
 
     var annotationIndex;
@@ -159,7 +140,7 @@ var Scroller = {
     angular.forEach(this.slides, function (slide) {
       slideDistance = currentY - slide.top;
 
-      // pin background slides
+      // pin background slides until change in background.
       if (slide.type === 'background') {
         if (currentY < slide.top) {
           slide.$inner.css({position: 'relative', top: 0});
@@ -199,48 +180,6 @@ var Scroller = {
         }
       }
     }, this);
-
-    return window.requestAnimationFrame(this.boundOnscroll);
-
-    // find current annotation and set current header
-    angular.forEach(this.annotationInfo, function (annotation, i) {
-      if (annotation !== this.currentAnnotation && currentY > annotation.top && currentY < annotation.bottom) {
-        annotation.header.removeClass('up down');
-        if (this.currentAnnotation.top < annotation.top) {
-          this.currentAnnotation.header.addClass('up');
-        } else {
-          this.currentAnnotation.header.addClass('down');
-        }
-        this.currentAnnotation = annotation;
-        console.log('current', annotation);
-      }
-    }, this);
-
-    slideDistance = (currentY - this.currentAnnotation.top) / this.height;
-
-    // set opacity of paragraph elements to distance from view
-    var distance;
-    var opacity;
-    angular.forEach(this.currentAnnotation.el.find('p'), function (paragraph, i) {
-      distance = Math.abs(this.currentAnnotation.paragraphs[i].top - currentY);
-      opacity = (Math.max(this.halfHeight - distance, 0) / this.halfHeight).toFixed(2);
-      angular.element(paragraph).css({opacity: opacity});
-    }, this);
-
-    // fade in titles
-    console.log('slide distance', slideDistance);
-    if (slideDistance < 1) {
-      this.currentAnnotation.authorEl.css({
-        display: 'block',
-        opacity: slideDistance
-      });
-    } else if (slideDistance < 2) {
-      this.currentAnnotation.authorEl.css({opacity: 2 - slideDistance});
-    } else {
-      this.currentAnnotation.authorEl.css({
-        display: 'none'
-      });
-    }
 
     return window.requestAnimationFrame(this.boundOnscroll);
   }
