b7ff9eb2ac6ad94db4a77f2c40a6bb5f9991ffb6
Kyle Warren <qarren@gmail.com>
2014-09-27 17:18:22 -0700

    added more pages

--- app/scripts/controllers/main-ctrl.js
+++ app/scripts/controllers/main-ctrl.js
@@ -16,6 +16,24 @@ angular.module('shotbyshot')
         description: 'Diego Echeverria\'s film, now restored, following the story of five members from Southside community.',
         subtitle: 'Coming Soon.'
       },
+      {
+        src: 'http://cf.lossur.es/home/HOME03',
+        title: 'Shot by Shot',
+        description: 'The film from 1984, dismantled shot by shot, so longstanding residents of Los Sures can splice in their own stories.',
+        play: true,
+      },
+      {
+        src: 'http://cf.lossur.es/home/HOME04',
+        title: '89 Steps',
+        description: 'An interactive story about Marta, one of the primary voices from the film, as she contemplates leaving the neighborhood.',
+        play: true,
+      },
+      {
+        src: 'http://cf.lossur.es/home/HOME05',
+        title: 'Southside Short Docs',
+        description: 'A collection of award-winning short documentaries inspired by the film from 1984 and the neighborhood today.',
+        play: true,
+      },
     ];
 
     shot.slides = [];
